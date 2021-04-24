89f94d963b65c74a059c5d7eb518dd38b87b2c5e
Denis Perchenko <plachenko@gmail.com>
2021-04-24 04:26:34 -0400

    adding target

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="capture" ref="capture" />
+    <Arm :position="position" />
+    <Target :position="{x: 100, y: 100}" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import Arm from './components/Arm.vue';
+import Target from './components/Target.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Arm,
+    Target
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+  $refs!: {
+    capture: HTMLElement
+  }
+
+  position: {x: number, y: number} = {x: 0, y: 0};
+
+  mounted(){
+    const el: HTMLElement = this.$refs.capture
+
+    el.addEventListener('mousemove', (e)=> {
+      let top = e.offsetY;
+      let left = e.offsetX;
+
+      this.position = {x: left, y: top}
+    });
+  }
+
+}
 </script>
 
 <style>
+*{
+  user-select: none;
+  overscroll-behavior: none;
+}
+html, body{
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+  touch-action: none;
+}
 #app {
+  overflow: hidden;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color:#FFF;
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  cursor: none;
+  }
+
+#capture{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: 9999;
 }
 </style>
---
+++ src/components/Target.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <div :style="styleObjInner" ref="target" id="inner" class="circ"></div>
+    <div :style="styleObj" ref="target" class="circ"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Target extends Vue {
+  $refs!: {
+    target: HTMLElement
+  }
+  @Prop() private position!: any;
+  mounted(){
+    this.$nextTick(()=>{
+      const el = this.$refs.target;
+
+      el.style.left = this.position.x + "px";
+      el.style.top = this.position.y + "px";
+    })
+  }
+}
+/*
+export default defineComponent({
+  name: 'Target',
+  props: {
+    pos: Object,
+  },
+  computed: {
+    styleObj(): string{
+      let pos = this.pos || {x: 0, y: 0, hit: false};
+      return `top: ${pos.x}px; left: ${pos.y}px;`;
+    },
+    styleObjInner(): string{
+      let pos = this.pos || {x: 0, y: 0, hit: false};
+      let size = 10;
+
+      let offset = {
+        x: pos.x + size,
+        y: pos.y + size
+      }
+
+      return `top: ${offset.x}px; left: ${offset.y}px;`;
+    }
+  },
+  data: function(){
+    return {
+      amtHit: 0
+    }
+  },
+  methods:{
+    onHit(amt = 1){
+      this.amtHit += amt;
+      this.$emit('on-hit', this);
+    },
+    animate(){
+      const el: HTMLElement = this.$refs.target as HTMLElement;
+      const speed = 4;
+      let left: number = parseInt(el.style.left);
+      let leftAmt = 0;
+      let opacityAmt = 0;
+
+      if(this.pos){
+
+        let xPos = this.pos.x || 0;
+
+        setInterval(()=>{
+          if(left <= (xPos-left) + 10 || left >= xPos + 20){
+            opacityAmt -= .05;
+            leftAmt = ( left += speed );
+          }else{
+            opacityAmt += .04;
+            leftAmt = ( left += speed/40 );
+          }
+
+          el.style.opacity = opacityAmt+"";
+          el.style.left = ( leftAmt )+"px";
+        }, 10);
+      }
+    }
+  },
+  mounted(){
+    // console.log(this.k)
+  }
+});
+*/
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.circ{
+  position: absolute;
+  height: 30px;
+  width: 30px;
+  border-radius: 30px;
+  z-index: 9998;
+  background-color:#F00;
+  /* opacity: 0; */
+}
+#inner{
+  background-color:#FF0;
+  height: 10px;
+  width: 10px;
+  z-index: 9999;
+}
+</style>
